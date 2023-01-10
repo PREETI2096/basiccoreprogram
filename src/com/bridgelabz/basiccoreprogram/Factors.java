57104720625ff6ea86bbb1f4e12b2e34de0afde9
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 14:36:40 +0530

    Added : basic core java programs

--- src/com/bridgelabz/basiccoreprogram/Factors.java
+++ src/com/bridgelabz/basiccoreprogram/Factors.java
@@ -3,8 +3,18 @@ package com.bridgelabz.basiccoreprogram;
 public class Factors {
 
 	public static void main(String[] args) {
-		
-
-	}
-
+	int number;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter a number:");
+	number = sc.nextInt();
+	System.out.println("Prime factors of entered number are:");
+	for(int i = 2; i <= number;i ++) {
+	    while(number % i == 0){
+	       number = number / i;
+	   }
+	System.out.println(i + "");
+	  }
+    if(number >= 2) 
+    	System.out.println(number);
+}
 }
