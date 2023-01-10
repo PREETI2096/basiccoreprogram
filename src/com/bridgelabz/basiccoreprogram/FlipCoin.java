bb50f6cd8dbf3ab36c7f98661d590db3e212b88d
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 14:39:45 +0530

    Added : basic core java programs

--- src/com/bridgelabz/basiccoreprogram/FlipCoin.java
+++ src/com/bridgelabz/basiccoreprogram/FlipCoin.java
@@ -1,10 +1,26 @@
 package com.bridgelabz.basiccoreprogram;
-
 public class FlipCoin {
 
 	public static void main(String[] args) {
-		
+		 int coinFlip;
+        int heads = 0;
+        int tails = 0;
+        System.out.print("Enter the number of times to flip the coin(Positive number): ");
+        Scanner sc = new Scanner(System.in);
+        coinFlip = sc.nextInt();
+        for(int i = 0; i < coinFlip; i++){
+            if(Math.random() < 0.5) {
+                heads++;
+            }
+            else {
+                tails++;
+            }
+        }
+        System.out.println("Number of Head flips: "+heads+"\nNumber of Tail flips: "+tails);
+        //Calculating percentage
+        double tailCount = tails / (double) coinFlip* 100 ;
+        double headCount = heads/ (double) coinFlip* 100 ;
+        System.out.println("Head Percentage: "+headCount+"\nTail Percentage: "+tailCount);
 
 	}
-
 }
